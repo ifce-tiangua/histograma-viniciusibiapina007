c0d467d8820211f33fe8d3e208a1cdbda2f6059c
viniciusibiapina007 <viniciusaluno2020@gmail.com>
2024-11-21 00:32:37 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,31 +5,30 @@ void preencher(char* his[], int valor, int categoria );
 void Imprimir_Histograma(char* his[], int valor[], int categoria);
 
 int main(){
-
     
-    int qtd_categ, i;
+    int qtd_categ, i; // quantidade de categorias
     scanf("%d", &qtd_categ);
 
-    char *histograma[qtd_categ];
-    int vendasPorCateg[qtd_categ];
+    char *histograma[qtd_categ]; // vetor de ponteiros para char
+    int vendasPorCateg[qtd_categ]; // numero de ventas por caategoria
 
     for(i=0; i<qtd_categ; i++){
 
-        scanf("%d", &vendasPorCateg[i]);
+        scanf("%d", &vendasPorCateg[i]); // para cada categoria de indice i vai ler um valor
         
         if(vendasPorCateg[i] == 0){
             histograma[i] = NULL;
         }
         else{
-            histograma[i] = (char *) malloc(vendasPorCateg[i]* sizeof(char));
-            preencher(histograma, vendasPorCateg[i], i);
+            histograma[i] = (char *) malloc(vendasPorCateg[i]* sizeof(char)); // criar um veto dinamico na posicao i do histograma
+            preencher(histograma, vendasPorCateg[i], i); // preencher este vetor dinamico com os asteristicos
         }
 
     }
-    Imprimir_Histograma(histograma, vendasPorCateg, qtd_categ);
+    Imprimir_Histograma(histograma, vendasPorCateg, qtd_categ);  // imprime o histograma
 
     for(i=0; i<qtd_categ; i++){
-        free(histograma[i]);
+        free(histograma[i]); // desalocar a memoria reservada para cada vetor dinamico do histograma
     }
     
     return 0;
